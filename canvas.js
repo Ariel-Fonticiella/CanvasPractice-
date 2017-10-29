dae6c6e6b064b8b028efde012f7c134ed9651d4e
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-10-29 15:41:38 -0400

    Added even listener to track cursor. Got circles to interact with mouse.

--- canvas.js
+++ canvas.js
@@ -40,8 +40,28 @@ var c = canvas.getContext('2d');
 //   c.stroke();
 // }
 
-window.addEventListener('mousemove', function(){
-  console.log('Test');
+var mouse = {
+  x: undefined,
+  y: undefined
+};
+
+var maxRadius = 40;
+var minRadius = 3;
+
+var colorArray =[
+  '#ffaa33',
+  '#99ffaa',
+  '#00ff00',
+  '#4411aa',
+  '#ff1100',
+];
+
+window.addEventListener('mousemove',
+    function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse);
+
 });
 
 function Circle(x, y, dx, dy, radius) {
@@ -61,21 +81,30 @@ function Circle(x, y, dx, dy, radius) {
     };
 
   this.update = function() {
-      if(this.x + this.radius > innerWidth || this.x - this.radius < 0)
-        {
+      if(this.x + this.radius > innerWidth || this.x - this.radius < 0) {
         this.dx = -this.dx;
-        }
+      }
 
-      if(this.y + this.radius > innerHeight || this.y - this.radius < 0)
-        {
+      if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
         this.dy = -this.dy;
-        }
+      }
 
-        this.x += this.dx;
-        this.y += this.dy;
+      this.x += this.dx;
+      this.y += this.dy;
+
+        // Interactivity Occurs Here
+
+      if(mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+           mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            if (this.radius < maxRadius) {
+                this.radius += 1;
+            }
+        } else if (this.radius > minRadius) {
+            this.radius -= 1;
+        }
 
         this.draw();
-  };
+    };
 }
 
 var circleArray = [];
@@ -84,8 +113,8 @@ for (var i = 0; i < 100; i++) {
     var radius = 30;
     var x = Math.random() * (innerWidth - radius * 2) + radius;
     var y = Math.random() * (innerHeight - radius * 2) + radius;
-    var dx = (Math.random() - 0.5) * 6;
-    var dy = (Math.random() - 0.5) * 6;
+    var dx = (Math.random() - 0.5) * 3;
+    var dy = (Math.random() - 0.5) * 3;
     circleArray.push(new Circle (x, y, dx, dy, radius));
 }
 
